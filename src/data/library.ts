46587a7957a712a26f45bf9c4df5a941ae256de6
ndsedf-stack <ndsedf@gmail.com>
2025-11-09 09:02:23 +0100

    Add exercise library with various exercises

---
+++ src/data/library.ts
@@ -0,0 +1,6 @@
+export const exerciseLibrary = [
+  { id: "bench_press", name: "Bench Press", muscles: ["Pectoraux","Triceps","Épaules"] },
+  { id: "squat", name: "Back Squat", muscles: ["Quadriceps","Fessiers","Bas du dos"] },
+  { id: "deadlift", name: "Deadlift", muscles: ["Ischio-jambiers","Fessiers","Dos"] },
+  // ...
+];
