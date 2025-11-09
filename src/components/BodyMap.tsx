2172c0f1bfdfefa13579c4fcc716d06d19a59be6
ndsedf-stack <ndsedf@gmail.com>
2025-11-09 09:27:10 +0100

    Add BodyMap component to display muscle groups

---
+++ src/components/BodyMap.tsx
@@ -0,0 +1,7 @@
+export default function BodyMap({ muscles }: { muscles: string[] }) {
+  return (
+    <div className="text-center text-sm text-gray-500">
+      Carte des muscles travaillés : {muscles.join(", ")}
+    </div>
+  )
+}
