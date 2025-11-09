5b89bbc04129bfa22209cd487c9cfb3395442375
ndsedf-stack <ndsedf@gmail.com>
2025-11-09 09:39:59 +0100

    Delete src/components/WorkoutCalendar.tsx

--- src/components/WorkoutCalendar.tsx
+++
@@ -1,7 +0,0 @@
-export default function WorkoutCalendar({ dates }: { dates: string[] }) {
-  return (
-    <div className="text-sm text-gray-500">
-      Jours d'entraînement : {dates.map((d) => <span key={d}>{d} • </span>)}
-    </div>
-  )
-}
