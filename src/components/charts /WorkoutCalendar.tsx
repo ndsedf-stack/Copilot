6a5ab9218e074fcc690442f2e5c1fdc66ed54cca
ndsedf-stack <ndsedf@gmail.com>
2025-11-09 09:40:16 +0100

    Update WorkoutCalendar.tsx

--- src/components/charts /WorkoutCalendar.tsx
+++ src/components/charts /WorkoutCalendar.tsx
@@ -1,1 +1,7 @@
-
+export default function WorkoutCalendar({ dates }: { dates: string[] }) {
+  return (
+    <div className="text-sm text-gray-500">
+      Jours d'entraînement : {dates.map((d) => <span key={d}>{d} • </span>)}
+    </div>
+  )
+}
